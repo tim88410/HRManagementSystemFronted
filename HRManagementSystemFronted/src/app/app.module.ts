1e6c6a366728bbd9e71d3bbd713f07424dd7b493
timchen <tim88410@gmail.com>
2025-07-08 22:50:54 +0800

    feat:加入leavescomponent cd app ng generate component leaves

--- HRManagementSystemFronted/src/app/app.module.ts
+++ HRManagementSystemFronted/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeavesComponent } from './leaves/leaves.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeavesComponent
   ],
   imports: [
     BrowserModule,
