fc0a28569a6c7bda4ea69ef8979d656e4d76e0ae
timchen <tim88410@gmail.com>
2025-07-08 23:03:47 +0800

    feat:調整AppComponent 加入HttpClientModule， 移除AppComponent的頁面，修正為Root目錄 AppRoutingModule 加入LeavesComponent的Route

--- HRManagementSystemFronted/src/app/app-routing.module.ts
+++ HRManagementSystemFronted/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LeavesComponent } from './leaves/leaves.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'leaves/:id', component: LeavesComponent },
+  { path: '**', redirectTo: 'leaves/1' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
